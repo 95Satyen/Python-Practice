dac14564f89754a7c854e79b6e3e31a01d915fba
95Satyen <35659334+95Satyen@users.noreply.github.com>
2018-03-02 20:30:54 +0530

    Automate the Boring Stuff with Python Programming

---
+++ Automate the boring stuff with Python Programming/Basics/intro.py
@@ -0,0 +1,12 @@
+# This program asks for personal details
+
+
+print('Hello')
+print('What is your name?') # ask for their name
+personName = input()
+print('Hi '+ personName)
+print('The length of your name is:')
+print(len(personName))
+print('What is your age') #ask for their age
+personAge = input()
+print('You will be ' +str(int(personAge)+1) + ' in a year')
